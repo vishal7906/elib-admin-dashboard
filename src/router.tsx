25791418d0c45b1df6958c02a334e39315e07af5
Vishal7906 <viratrmr@gmail.com>
2024-07-14 18:19:57 +0530

    added layout

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,24 @@ import {createBrowserRouter} from 'react-router-dom';
 import LoginPage from './pages/Login';
 import HomePage from './pages/HomePage';
 import Register from './pages/Register';
+import DashboardLayout from './layouts/DashboardLayout';
+import BookPage from './pages/BookPage';
 
 export const router = createBrowserRouter([
     {
-      path: "/",
-      element: <HomePage />
+      path: "dashboard",
+      element: <DashboardLayout />,
+      children:[
+      {
+        path:"home",
+        element:<HomePage/>
+      },
+      {
+        path:"books",
+        element:<BookPage/>
+      }
+      ],
+      
     },
     {
         path:"/login",
