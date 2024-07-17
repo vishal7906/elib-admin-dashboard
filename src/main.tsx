449b0300f895e73ccb87666e6f7161aff28bac6a
Vishal7906 <viratrmr@gmail.com>
2024-07-17 16:13:16 +0530

    add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { router } from './router'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryclient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryclient}>
     <RouterProvider router={router}/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,9 +2,20 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
+
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLogin = ()=>{
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log("data ",{email,password});
+    
+  }
   return (
     <section className="flex items-center justify-center h-screen">
         <Card className="w-full max-w-sm">
@@ -17,16 +28,16 @@ const LoginPage = () => {
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
+            <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
             <div className="w-full">
-            <Button className="w-full">Sign in</Button>
+            <Button onClick={handleLogin} className="w-full">Sign in</Button>
             <div className="mt-4 text-center text-sm">
             Already have an account?{" "}
             <Link to={'/auth/register'} className="underline">
